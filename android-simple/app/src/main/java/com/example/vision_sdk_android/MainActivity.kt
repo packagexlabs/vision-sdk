949f199bb4dfbf7874b3a6ae4518bf297126cb0f
Abdul Basit <abdulbasit5361234@gmail.com>
2022-12-31 10:48:32 +0500

    Project created

---
+++ android-simple/app/src/main/java/com/example/vision_sdk_android/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.vision_sdk_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
