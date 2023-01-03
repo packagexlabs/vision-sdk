5d31cc3d9f1fe622f6458b48649ac2cfbbc73045
Abdul Basit <abdulbasit5361234@gmail.com>
2023-01-03 20:16:08 +0500

    Simple code updated

--- android-simple/app/src/main/java/com/example/vision_sdk_android/ScanMode.kt
+++ android-simple/app/src/main/java/com/example/vision_sdk_android/ScanMode.kt
@@ -1,27 +1,12 @@
 package com.example.vision_sdk_android
 
+import com.example.customscannerview.mlkit.enums.ViewType
+import com.example.customscannerview.mlkit.views.DetectionMode
+
 
 data class ScreenState(
-    val scanningMode: ScanningMode = ScanningMode.Manual,
-    val selectedMode: SelectedMode = SelectedMode.Barcode,
-    val windowSize: WindowSize = WindowSize.Window,
+    val scanningWindow: ViewType = ViewType.RECTANGLE,
+    val detectionMode: DetectionMode = DetectionMode.Barcode,
+    val scanningMode: com.example.customscannerview.mlkit.views.ScanningMode = com.example.customscannerview.mlkit.views.ScanningMode.Manual,
     val flashStatus: Boolean = false
 )
-
-
-interface SelectedMode {
-    object Barcode : SelectedMode
-    object QRCode : SelectedMode
-    object OCR : SelectedMode
-}
-
-interface WindowSize {
-    object Window : WindowSize
-    object FullScreen : WindowSize
-}
-
-interface ScanningMode {
-    object Auto : ScanningMode
-    object Manual : ScanningMode
-}
-
