295f9c4d302ee84c48f8564644f682cfe38bb1ff
muzamil8101 <92907344+muzamil8101@users.noreply.github.com>
2025-03-11 21:26:43 +0500

    SPM Configuration Updated

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "VisionSDK",
-            targets: ["VisionSDK", "PXVisionSDK", "TensorFlow"]),
+            targets: ["VisionSDK", "TensorFlow"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -22,16 +22,8 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         
-        .target(
-            name: "VisionSDK",
-            dependencies: ["PXVisionSDK", "TensorFlow"],
-            linkerSettings: [
-                .linkedLibrary("c++")
-            ]
-        ),
-        
         .binaryTarget(
-                    name: "PXVisionSDK",
+                    name: "VisionSDK",
                     path: "./Sources/VisionSDK.xcframework"),
         .binaryTarget(
                     name: "TensorFlow",
--- Sources/VisionSDK/VisionSDK.swift
+++
@@ -1,7 +0,0 @@
-//
-//  VisionSDK.swift
-//  VisionSDK
-//
-//  Created by Muzamil Yasin on 11/03/2025.
-//
-
